af0bd712e22f97a1bd0b66327f1400316926fc8d
BH LIM <47635533+bhlim92@users.noreply.github.com>
2026-07-02 06:53:16 +0900

    clean: Remove all legacy Python and Web files to enforce 100% pure C# workspace

--- Aura3DRobotConverter/obj/Debug/net9.0-windows/Aura3DRobotConverter.AssemblyInfo.cs
+++ Aura3DRobotConverter/obj/Debug/net9.0-windows/Aura3DRobotConverter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aura3DRobotConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1528e8fb17bec710ef22669ff869021ff513264")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b21bf0e5bec16d584efc824d6098d7ad41eb175f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aura3DRobotConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aura3DRobotConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
