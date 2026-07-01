546cfdcbc75e48bd5522aa6c859af1d7ebb39fa2
BH LIM <47635533+bhlim92@users.noreply.github.com>
2026-07-01 21:08:36 +0900

    style: Redesign UI to Windows 11 Fluent Design System Light Theme

--- Aura3DRobotConverter/obj/Debug/net9.0-windows/Aura3DRobotConverter.AssemblyInfo.cs
+++ Aura3DRobotConverter/obj/Debug/net9.0-windows/Aura3DRobotConverter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aura3DRobotConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a9906e73662b8f45315fc06e3a6d286c1a7dfcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919139c606cd38396a8f9c2687cb7366b4a72c79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aura3DRobotConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aura3DRobotConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
