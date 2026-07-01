5f44a8e65b5efb903dddc76516af23f6ddddbbb5
BH LIM <47635533+bhlim92@users.noreply.github.com>
2026-07-01 20:43:23 +0900

    style: Add DefaultLights to HelixViewport3D for realistic 3D mesh rendering

--- Aura3DRobotConverter/obj/Debug/net9.0-windows/Aura3DRobotConverter.AssemblyInfo.cs
+++ Aura3DRobotConverter/obj/Debug/net9.0-windows/Aura3DRobotConverter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aura3DRobotConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b732cef806d78e3e2bb17f88b15f9a612844592")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32143369433924f086224ac14fa7539024d5c944")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aura3DRobotConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aura3DRobotConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
